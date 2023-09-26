934519b1e4344623d01fe787220f8bc62d92dc8b
Hoang Minh Tu <tu.hoangminh15@gmail.com>
2023-09-26 20:50:11 +0700

    new commit

--- main.js
+++ main.js
@@ -1,22 +1,3 @@
-// Write a function when user click on button it will change the background color of box1 to red.
-var counter = document.getElementById("counter");
-
-var btn = document.getElementById("btn").addEventListener("click", function () { 
-    counter.textContent = parseInt(counter.textContent) + 1;
-    
-    if(counter.textContent == 21) {
-        document.getElementById("theLe-img").style.display = "block";
-    }
-    else {
-        document.getElementById("theLe-img").style.display = "none";
-    }
-});
-
-document.getElementById("reset-btn").addEventListener("click", function () {
-    counter.textContent = 0;
-    document.getElementById("theLe-img").style.display = "none";
-});
-
 
 
 
